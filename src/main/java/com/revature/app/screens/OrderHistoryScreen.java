721f3b73fd11f02f8c53ed307bc34b08a6c0e3f8
nuttdguy <pygnasakdev@gmail.com>
2023-05-28 14:20:56 -0500

    Refactor and add logging to OrderHistory
    - refactor various methods in OrderHistory
    - added logger to entry of methods

--- src/main/java/com/revature/app/screens/OrderHistoryScreen.java
+++ src/main/java/com/revature/app/screens/OrderHistoryScreen.java
@@ -5,6 +5,8 @@ import com.revature.app.models.Session;
 import com.revature.app.services.OrderService;
 import com.revature.app.services.RouterService;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.Scanner;
@@ -13,6 +15,8 @@ import static java.lang.System.out;
 
 @AllArgsConstructor
 public class OrderHistoryScreen implements IScreen {
+    private static final Logger logger = LogManager.getLogger(OrderHistoryScreen.class);
+
     private final OrderService orderService;
 
     private final RouterService routerService;
@@ -22,61 +26,86 @@ public class OrderHistoryScreen implements IScreen {
     @Override
     public void start(Scanner scanner) {
 
-        exit: {
-            while (true) {
+        while (true) {
 
-                out.println("What would you like to do?");
-                out.println("[1] View current cart");
-                out.println("[2] View completed orders");
-                out.println("[x] or any key to go back to the storefront");
-                String input = scanner.nextLine();
+            out.println("What would you like to do?");
+            out.println("[1] View current cart");
+            out.println("[2] View completed orders");
+            out.println("[x] or any key to go back to the storefront");
+            String input = scanner.nextLine();
 
-                switch (input) {
-                    case "1":
-                        List<Order> orderItems = orderService.findOrderByUserId(session.getId(), OrderService.ORDER_STATUS.PENDING);
-
-                        if (orderItems.size() > 0) {
-                            clearScreen();
-                            displayOrderItems(orderItems);
-                            displayOrderTotal(orderItems);
-                        } else {
-                            out.println("Unable to find a current cart");
-                        }
-                        out.println("Press any key to continue...");
-                        scanner.nextLine();
-                        continue;
-                    case "2":
-                        orderItems = orderService.findOrderByUserId(session.getId(), OrderService.ORDER_STATUS.COMPLETE);
-
-                        if (orderItems.size() > 0) {
-                            clearScreen();
-                            displayOrderItems(orderItems);
-                            displayOrderTotal(orderItems);
-                        } else {
-                            out.println("Unable to find any orders");
-                        }
-                        out.println("Press any key to continue...");
-                        scanner.nextLine();
-                        continue;
-                    default:
-                        routerService.navigate("/storefront", scanner);
-
+            switch (input) {
+                case "1" -> {
+                    viewCurrentOrder(scanner);
+                    continue;
                 }
-
-                break exit;
+                case "2" -> {
+                    viewCompletedOrders(scanner);
+                    continue;
+                } default -> {
+                    routerService.navigate("/storefront", scanner);
+                }
             }
+            break;
         }
     }
 
     /*
-     * ------------------------  Helper methods ------------------------
+     * ------------------------  Screen helper methods ------------------------
+     */
+
+    /* Fetches orders that are currently pending / open and displays them
+    *
+    * @param scanner scans for user input
+    * */
+    private void viewCurrentOrder(Scanner scanner) {
+        logger.info("Entering into viewCurrentOrder(Scanner scanner)");
+
+        List<Order> orderItems = orderService.findOrderByUserId(session.getId(), OrderService.ORDER_STATUS.PENDING);
+
+        if (orderItems.size() > 0) {
+            clearScreen();
+            displayOrderItems(orderItems);
+            displayOrderTotal(orderItems);
+        } else {
+            out.println("Unable to find a current cart");
+        }
+        out.println("Press any key to continue...");
+        scanner.nextLine();
+    }
+
+    /* Fetches orders that are currently complete / closed and displays them
+     *
+     * @param scanner scans for user input
+     * */
+    private void viewCompletedOrders(Scanner scanner) {
+        logger.info("Entering into viewCompletedOrders(Scanner scanner)");
+
+        List<Order> orderItems = orderService.findOrderByUserId(session.getId(), OrderService.ORDER_STATUS.COMPLETE);
+
+        if (orderItems.size() > 0) {
+            clearScreen();
+            displayOrderItems(orderItems);
+            displayOrderTotal(orderItems);
+        } else {
+            out.println("Unable to find any orders");
+        }
+        out.println("Press any key to continue...");
+        scanner.nextLine();
+    }
+
+    /*
+     * ------------------------  Display helper methods ------------------------
      */
 
 
-    /* Display the formatted order contents of the list
+    /* Displays a formatted contents of the items / products in the list
+     *
+     * @param orderItems a list of items in an order
      *
      * */
     private void displayOrderItems(List<Order> orderItems) {
+        logger.info("Entering displayOrderItems(List<Order> orderItems)");
 
         // Display the items in the order
         orderItems.stream()
@@ -93,9 +122,14 @@ public class OrderHistoryScreen implements IScreen {
 
     }
 
-    /* Sums up and displays the order total
-     *  */
+    /* Sums up the product price and order quantity and displays the value as the order total
+     *
+     * @param orderItems a list of items in an order
+     *
+     * */
     private void displayOrderTotal(List<Order> orderItems) {
+        logger.info("Entering displayOrderTotal(List<Order> orderItems)");
+
         double total = 0.00;
         for (Order item : orderItems) {
             total += toDouble(item.getProduct().getPrice()) * toDouble(item.getQuantity());
